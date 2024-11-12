e5ea88ef9be6496cd187af50c6257eb158311ca2
Missael Burciaga Hdz <missaelburciaga37@gmail.com>
2024-11-11 22:36:46 -0600

    Mejoré un poco la arquitectura del proyecto

---
+++ Model/pandasModel.py
@@ -0,0 +1,26 @@
+from PySide6.QtCore import QAbstractTableModel, Qt
+
+
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self._data = data
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid() and role == Qt.DisplayRole:
+            return str(self._data.iloc[index.row(), index.column()])
+        return None
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
+        return None
--- ViewModel/BilboMainViewModel.py
+++ ViewModel/BilboMainViewModel.py
@@ -1,34 +1,8 @@
 # ViewModel/BilboMainViewModel.py
-from PySide6.QtCore import QAbstractTableModel, Qt
 from Model.fondosModel import FondosModel
 from View.RegistrarBilboView import Ui_widget
 from PySide6.QtWidgets import QDialog
-
-
-class PandasModel(QAbstractTableModel):
-    def __init__(self, data):
-        super().__init__()
-        self._data = data
-
-    def rowCount(self, index):
-        return self._data.shape[0]
-
-    def columnCount(self, index):
-        return self._data.shape[1]
-
-    def data(self, index, role=Qt.DisplayRole):
-        if index.isValid() and role == Qt.DisplayRole:
-            return str(self._data.iloc[index.row(), index.column()])
-        return None
-
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
-        if role == Qt.DisplayRole:
-            if orientation == Qt.Horizontal:
-                return str(self._data.columns[section])
-            if orientation == Qt.Vertical:
-                return str(self._data.index[section])
-        return None
-
+from Model.pandasModel import PandasModel
 
 class BilboMainViewModel:
     def __init__(self, db_path):
